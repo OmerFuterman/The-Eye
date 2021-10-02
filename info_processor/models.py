f57d53c6911de8ff2c9f4ac2223431155ee70983
OmerFuterman <omerfuterman@gmail.com>
2021-10-02 18:33:07 -0400

    make model, make migrations, add rest framework

--- info_processor/models.py
+++ info_processor/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class event(models.Model):
+    session_id = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    meta_data = models.JSONField()
+    timestamp = models.DateField()
--- the_eye/settings.py
+++ the_eye/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5n0g8p=kbl8h+9&^(7gcnu67^$2vm1n=52kpytp(i6j-etn3-t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'info_processor'
 ]
 
 MIDDLEWARE = [
