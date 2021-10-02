acb0716996f232dbbaabece1083c82746fcfb219
OmerFuterman <omerfuterman@gmail.com>
2021-10-02 17:54:58 -0400

    add empty view

--- info_processor/views.py
+++ info_processor/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def add_info(request):
+    return HttpResponse('I am working')
